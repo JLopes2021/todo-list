e3bb253928e4c3db940a385996bddee3a01444aa
JLopes2021 <jeffinho.je8@gmail.com>
2023-09-26 18:28:01 -0300

    versao-andamento

--- index.js
+++ index.js
@@ -1,81 +1,128 @@
 document.addEventListener('DOMContentLoaded', function() {
     const newTaskInput = document.getElementById('newTask');
     const taskList = document.getElementById('taskList');
+    const inProgressList = document.getElementById('inProgressList');
     const completedList = document.getElementById('completedList');
 
-    // Adicionando o botão "Apagar Tudo" via JavaScript
-    const clearButton = document.createElement('button');
-    clearButton.textContent = 'Apagar Tudo';
-    clearButton.addEventListener('click', clearAllTasks);
-    document.body.appendChild(clearButton);
+    function addTask(taskText) {
+        const tasks = taskText.split(';');
 
-     function addTask() {
-        const taskText = newTaskInput.value.trim();
+        tasks.forEach(task => {
+            if (task.trim() !== '') {
+                const li = document.createElement('li');
+                const now = new Date(); 
+                const timestamp = now.toLocaleString(); 
 
-        if (taskText !== '') {
-            const tasks = taskText.split(';');
+                const taskElement = document.createElement('span');
+                taskElement.classList.add('task');
+                taskElement.textContent = task.trim(); 
 
-            tasks.forEach(task => {
-                if (task.trim() !== '') {
-                    const li = document.createElement('li');
-                    const now = new Date(); // Obtém a data e hora atual
-                    const timestamp = now.toLocaleString(); // Formata a data e hora
+                const timestampElement = document.createElement('span');
+                timestampElement.classList.add('timestamp');
+                timestampElement.textContent = ` - Adicionado em ${timestamp}`;
 
-                    const taskElement = document.createElement('span');
-                    taskElement.classList.add('task');
-                    taskElement.textContent = task;
+                li.appendChild(taskElement);
+                li.appendChild(timestampElement);
 
-                    const timestampElement = document.createElement('span');
-                    timestampElement.classList.add('timestamp');
-                    timestampElement.textContent = ` - ${timestamp}`;
+                li.innerHTML += `
+                    <button class="remove">Remover</button>
+                    <button class="completeButton">Concluído</button>
+                    <button class="inProgressButton">Em Andamento</button>
+                `;
 
-                    li.appendChild(taskElement);
-                    li.appendChild(timestampElement);
+                taskList.appendChild(li);
 
-                    li.innerHTML += `
-                        <button class="remove">Remover</button>
-                        <button class="completeButton">Concluído</button>
-                    `;
+                li.querySelector('.remove').addEventListener('click', removeTask);
+                li.querySelector('.completeButton').addEventListener('click', completeTask);
+                li.querySelector('.inProgressButton').addEventListener('click', moveInProgress);
+            }
+        });
 
-                    taskList.appendChild(li);
-
-                    li.querySelector('.remove').addEventListener('click', removeTask);
-                    li.querySelector('.completeButton').addEventListener('click', toggleComplete);
-                }
-            });
-
-            newTaskInput.value = '';
-        }
+        newTaskInput.value = '';
     }
-
     function removeTask() {
         this.parentElement.remove();
     }
 
-    function toggleComplete() {
-        this.parentElement.children[0].classList.toggle('complete');
+    function completeTask() {
+        const now = new Date(); 
+        const timestamp = now.toLocaleString(); 
 
-        if (this.parentElement.parentElement === taskList) {
-            // A tarefa está na lista de tarefas
-            completedList.appendChild(this.parentElement);
-            this.parentElement.querySelector('.remove').remove();
-            this.remove();
-        } else {
-            // A tarefa está na lista de tarefas concluídas
-            taskList.appendChild(this.parentElement);
-            this.parentElement.querySelector('.completeButton').remove();
-            this.remove();
-        }
+        this.parentElement.children[0].classList.toggle('complete');
+        completedList.appendChild(this.parentElement);
+
+        // Remove todos os botões de ação
+        const actionButtons = this.parentElement.querySelectorAll('.remove, .completeButton, .inProgressButton');
+        actionButtons.forEach(button => button.remove());
+
+        const timestampElement = document.createElement('span');
+        timestampElement.classList.add('timestamp');
+        timestampElement.textContent = ` - Concluído em ${timestamp}`;
+        this.parentElement.appendChild(timestampElement);
+    }
+
+    function moveInProgress() {
+        inProgressList.appendChild(this.parentElement);
+
+        // Remove o botão "Em Andamento"
+        this.parentElement.querySelector('.inProgressButton').remove();
+
+        // Remove os botões de ação da lista concluída
+        const completedButtons = this.parentElement.querySelectorAll('.completeButton, .remove');
+        completedButtons.forEach(button => button.remove());
+
+        const now = new Date(); 
+        const timestamp = now.toLocaleString(); 
+
+        const timestampElement = document.createElement('span');
+        timestampElement.classList.add('timestamp');
+        timestampElement.textContent = ` - Em Andamento em ${timestamp}`;
+        this.parentElement.appendChild(timestampElement);
     }
 
     function clearAllTasks() {
-        taskList.innerHTML = '';
-        completedList.innerHTML = '';
+        const senha = prompt('Digite a senha para apagar todas as tarefas:');
+
+        if(senha === 'senha'){
+            alert('Ok, apagamos todas as tarefas');
+            taskList.innerHTML = '';
+            inProgressList.innerHTML = '';
+            completedList.innerHTML = '';
+        } else {
+            alert('Senha Incorreta');
+        }
     }
+
+    document.getElementById('addTask').addEventListener('click', function() {
+        const taskText = newTaskInput.value.trim();
+        if (taskText !== '') {
+            addTask(taskText);
+            newTaskInput.value = '';
+        }
+    });
 
     newTaskInput.addEventListener('keyup', function(event) {
         if (event.key === 'Enter') {
-            addTask();
+            const taskText = newTaskInput.value.trim();
+            if (taskText !== '') {
+                addTask(taskText);
+                newTaskInput.value = '';
+            }
         }
     });
+
+    document.getElementById('clearAll').addEventListener('click', clearAllTasks);
+
+    function getCurrentTimestamp() {
+        const now = new Date(); 
+        return now.toLocaleString(); 
+    }
+
+    function updateClock() {
+        const clockElement = document.getElementById('clock');
+        clockElement.textContent = getCurrentTimestamp();
+        setTimeout(updateClock, 1000);
+    }
+
+    updateClock();
 });
